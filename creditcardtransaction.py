c8d120dc3ef07b6bd84f9f74b4e2e1de14b829dc
Petrus Maxmiliano <163488590+maxmiloiano@users.noreply.github.com>
2025-01-20 20:49:28 +0700

    Add files via upload

--- creditcardtransaction.py
+++ creditcardtransaction.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import StandardScaler, LabelEncoder
